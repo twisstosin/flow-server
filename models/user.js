f8273ac484a363f72692845db7061e84960d774e
Tosin Omotoyinbo <twisstosin@outlook.com>
2018-05-31 13:45:00 +0100

    finished user model setup

--- models/user.js
+++ models/user.js
@@ -1,13 +1,32 @@
 'use strict';
+
+import {bcrypt} from 'bcrypt'
+
 export default function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
     Name: DataTypes.STRING,
     Location: DataTypes.STRING,
     Username: DataTypes.STRING,
     Password: DataTypes.STRING
-  }, {});
+  }, {
+    hooks:{
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.Password = bcrypt.hashSync(user.Password, salt)
+      }
+    }
+  });
+
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+    };
+
   User.associate = function (models) {
     // associations can be defined here
   };
+
+  sequelize.sync()
+  .then(() => console.log("users table has been created, if one didn\'t exist"))
+  .catch(error => console.log("Error occured", error));
   return User;
 }
